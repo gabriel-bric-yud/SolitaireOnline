be8c3e0ac40edf9b6cdcdf44bc502f5d34c73dfe
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2023-11-12 04:43:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1246,13 +1246,12 @@ function createDraggable(elem, cardArray, cardDivArray, i ) {
       //offSet = [dragTarget.offsetLeft - e.touches[0].clientX, dragTarget.offsetTop - e.touches[0].clientY]  //#offSet in cardStack
       //dragTarget.style.left = e.touches[0].clientX - (dragTarget.offsetWidth/2) + 'px'
       //dragTarget.style.top = e.touches[0].clientY  - (dragTarget.offsetHeight/2) + 'px'
-      offSet = [(e.touches[0].clientX + window.screenX) - (dragTarget.offsetWidth/2), (e.touches[0].clientY + window.screenY) - (dragTarget.offsetHeight/1.2)]
   
-      dragTarget.style.left = offSet[0] + 'px'
-      dragTarget.style.top = offSet[1] + 'px'
+      dragTarget.style.left = (e.touches[0].clientX) - (dragTarget.offsetWidth/2) + 'px'
+      dragTarget.style.top = (e.touches[0].clientY) - (dragTarget.offsetHeight/1.2) + 'px'
       document.body.appendChild(dragTarget)
     
-      //offSet = [dragTarget.offsetLeft - e.touches[0].clientX, dragTarget.offsetTop - e.touches[0].clientY]
+      offSet = [dragTarget.offsetLeft - e.touches[0].clientX - window.screenX, dragTarget.offsetTop - e.touches[0].clientY - window.screenY]
       
       clicked = true 
     
