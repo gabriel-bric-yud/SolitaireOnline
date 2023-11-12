350f937dc4d4d3926cd92e0b238c1e668b318a6b
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2023-11-12 04:21:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1245,7 +1245,7 @@ function createDraggable(elem, cardArray, cardDivArray, i ) {
       //offSet = [dragTarget.offsetLeft - e.touches[0].clientX, dragTarget.offsetTop - e.touches[0].clientY]  //#offSet in cardStack
       //dragTarget.style.left = e.touches[0].clientX - (dragTarget.offsetWidth/2) + 'px'
       //dragTarget.style.top = e.touches[0].clientY  - (dragTarget.offsetHeight/2) + 'px'
-      offSet = [(e.touches[0].clientX + window.screenLeft) - (dragTarget.offsetWidth/2), (e.touches[0].clientY + window.screenTop) - (dragTarget.offsetHeight/4)]
+      offSet = [(e.touches[0].clientX + window.screenLeft) - (dragTarget.offsetWidth/2), (e.touches[0].clientY + window.screenTop) - (dragTarget.offsetHeight/1.2)]
   
       dragTarget.style.left = offSet[0] + 'px'
       dragTarget.style.top = offSet[1] + 'px'
